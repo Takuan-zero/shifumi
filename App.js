7e948c7f5c1cc2486bf19515c01f16f92eb626d5
Nicolas Godfrin <n.godfrin@gmail.com>
2019-01-29 21:06:03 +0100

    Main page and clean repo

--- App.js
+++ App.js
@@ -1,50 +1,9 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+import Main from './screens/HomeScreen';
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-      }),
-    ]);
-  };
-
-  _handleLoadingError = () => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
-
-  render() {
-    const { isLoadingComplete } = this.state;
-
-    // eslint-disable-next-line
-    if (!isLoadingComplete && !this.props.skipLoadingScreen) {
-      return <AppLoading />;
-    }
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      </View>
-    );
-  }
+function App() {
+  return <Main />;
 }
+
+export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,139 +1,65 @@
 import React from 'react';
-import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
-
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import Logo from '../assets/images/shifumi.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
+    backgroundColor: '#ced8e0',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'center',
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  h1: {
+    color: '#008F68',
+    fontSize: 70,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  h2: {
+    color: '#008F68',
+    fontSize: 18,
+    marginTop: 8,
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  buttonContainer: {
+    backgroundColor: '#008F68',
+    borderRadius: 5,
+    margin: 8,
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
+  topContainer: {
+    flex: 2,
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  helpLink: {
-    paddingVertical: 15,
+  middleContainer: {
+    flex: 3,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  bottomContainer: {
+    justifyContent: 'flex-end',
+    width: '90%',
+    margin: 20,
+    padding: 10,
   },
 });
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
-
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer} />
-
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer} />
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+        <View style={styles.topContainer}>
+          <Text style={styles.h1}>Shifumi Online</Text>
+          <Text style={styles.h2}>Best game ever made</Text>
+        </View>
+        <View style={styles.middleContainer}>
+          <Image source={Logo} style={styles.image} />
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.buttonContainer}>
+            <Button
+              title="LET'S START"
+              style={styles.button}
+              onPress={() => this.onPress()}
+              color="#008F68"
+            />
           </View>
         </View>
       </View>
