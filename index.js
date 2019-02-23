dbf2fa9973474eddbd433c9fc00a4808f9656a5e
Nicolas Godfrin <n.godfrin@gmail.com>
2019-02-24 00:35:13 +0100

    REMOVE redux from index.js

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
-import { Provider } from 'react-redux';
 import App from './App';
 import { name as appName } from './app.json';
 
-import configureStore from './store';
-
-const store = configureStore();
-
-const RNRedux = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const RNRedux = () => <App />;
 
 AppRegistry.registerComponent(appName, () => RNRedux);
